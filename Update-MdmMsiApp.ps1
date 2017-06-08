8863541b9b4258668cb19a01804f951dd2d6e4de
SAMUEL S. GRUMMONS <ssgrummo@us.ibm.com>
2017-06-08 15:03:31 -0700

    Add Script to Update MSI Package in InTune
    This script updates an MSI package in InTune

---
+++ Update-MdmMsiApp.ps1
@@ -0,0 +1,456 @@
+﻿
+####################FUNCTIONS#######################
+
+function Get-AuthToken {
+
+<#
+.SYNOPSIS
+This function is used to authenticate with the Graph API REST interface
+.DESCRIPTION
+The function authenticate with the Graph API Interface with the tenant name
+.EXAMPLE
+Get-AuthToken
+Authenticates you with the Graph API interface
+.NOTES
+NAME: Get-AuthToken
+#>
+
+[cmdletbinding()]
+
+param
+(
+    [Parameter(Mandatory=$true)]
+    $User
+)
+
+$userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
+
+$tenant = $userUpn.Host
+
+Write-Host "Checking for AzureAD module..."
+
+    $AadModule = Get-Module -Name "AzureAD" -ListAvailable
+
+    if ($AadModule -eq $null) {
+
+        Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
+        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
+
+    }
+
+    if ($AadModule -eq $null) {
+        write-host
+        write-host "AzureAD Powershell module not installed..." -f Red
+        write-host "Install by running 'Install-Module AzureAD' or 'Install-Module AzureADPreview' from an elevated PowerShell prompt" -f Yellow
+        write-host "Script can't continue..." -f Red
+        write-host
+        exit
+    }
+
+# Getting path to ActiveDirectory Assemblies
+# If the module count is greater than 1 find the latest version
+
+    if($AadModule.count -gt 1){
+
+        $Latest_Version = ($AadModule | select version | Sort-Object)[-1]
+
+        $aadModule = $AadModule | ? { $_.version -eq $Latest_Version.version }
+
+            # Checking if there are multiple versions of the same module found
+
+            if($AadModule.count -gt 1){
+
+            $aadModule = $AadModule | select -Unique
+
+            }
+
+        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+
+    }
+
+    else {
+
+        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+
+    }
+
+[System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
+
+[System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
+
+$clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
+ 
+$redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+ 
+$resourceAppIdURI = "https://graph.microsoft.com"
+ 
+$authority = "https://login.windows.net/$Tenant"
+ 
+    try {
+
+    $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+
+    # https://msdn.microsoft.com/en-us/library/azure/microsoft.identitymodel.clients.activedirectory.promptbehavior.aspx
+    # Change the prompt behaviour to force credentials each time: Auto, Always, Never, RefreshSession
+
+    $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
+
+    $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
+
+    $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
+
+        # If the accesstoken is valid then create the authentication header
+
+        if($authResult.AccessToken){
+
+        # Creating header for Authorization token
+
+        $authHeader = @{
+            'Content-Type'='application/json'
+            'Authorization'="Bearer " + $authResult.AccessToken
+            'ExpiresOn'=$authResult.ExpiresOn
+            }
+
+        return $authHeader
+
+        }
+
+        else {
+
+        Write-Host
+        Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
+        Write-Host
+        break
+
+        }
+
+    }
+
+    catch {
+
+    write-host $_.Exception.Message -f Red
+    write-host $_.Exception.ItemName -f Red
+    write-host
+    break
+
+    }
+
+}
+ 
+####################################################
+
+Function Get-IntuneApplication(){
+
+<#
+.SYNOPSIS
+This function is used to get applications from the Graph API REST interface
+.DESCRIPTION
+The function connects to the Graph API Interface and gets any applications added
+.EXAMPLE
+Get-IntuneApplication
+Returns any applications configured in Intune
+.NOTES
+NAME: Get-IntuneApplication
+#>
+
+[cmdletbinding()]
+
+param
+(
+    $Name
+)
+
+$graphApiVersion = "Beta"
+$Resource = "deviceAppManagement/mobileApps"
+
+    try {
+
+        if($Name){
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value | Where-Object { ($_.'displayName').contains("$Name") -and (!($_.'@odata.type').Contains("managed")) -and (!($_.'@odata.type').Contains("#microsoft.graph.iosVppApp")) }
+
+        }
+
+        else {
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        (Invoke-RestMethod -Uri $uri –Headers $authToken –Method Get).Value | Where-Object { (!($_.'@odata.type').Contains("managed")) -and (!($_.'@odata.type').Contains("#microsoft.graph.iosVppApp")) }
+
+        }
+
+    }
+
+    catch {
+
+    $ex = $_.Exception
+    Write-Host "Request to $Uri failed with HTTP Status $([int]$ex.Response.StatusCode) $($ex.Response.StatusDescription)" -f Red
+    $errorResponse = $ex.Response.GetResponseStream()
+    $reader = New-Object System.IO.StreamReader($errorResponse)
+    $reader.BaseStream.Position = 0
+    $reader.DiscardBufferedData()
+    $responseBody = $reader.ReadToEnd();
+    Write-Host "Response content:`n$responseBody" -f Red
+    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+    write-host
+    break
+
+    }
+
+}
+
+####################################################
+
+Function Test-JSON(){
+
+<#
+.SYNOPSIS
+This function is used to test if the JSON passed to a REST Post request is valid
+.DESCRIPTION
+The function tests if the JSON passed to the REST Post is valid
+.EXAMPLE
+Test-JSON -JSON $JSON
+Test if the JSON is valid before calling the Graph REST interface
+.NOTES
+NAME: Test-AuthHeader
+#>
+
+param (
+
+$JSON
+
+)
+
+    try {
+
+    $TestJSON = ConvertFrom-Json $JSON -ErrorAction Stop
+    $validJson = $true
+
+    }
+
+    catch {
+
+    $validJson = $false
+    $_.Exception
+
+    }
+
+    if (!$validJson){
+
+    Write-Host "Provided JSON isn't in valid JSON format" -f Red
+    break
+
+    }
+
+}
+
+####################################################
+
+Function Update-InTuneApplication(){
+
+<#
+.SYNOPSIS
+This function is used to update an MSI application using the Graph API REST interface
+.DESCRIPTION
+The function connects to the Graph API Interface and updates an MSI package 
+.EXAMPLE
+Add-AndroidApplication -JSON $JSON -IconURL pathtourl
+Adds an Android application into Intune using an icon from a URL
+.NOTES
+NAME: Add-AndroidApplication
+#>
+
+[cmdletbinding()]
+
+param
+(
+    $JSON,
+    $appID,
+    $IconURL
+)
+
+$graphApiVersion = "Beta"
+$App_resource = "deviceAppManagement/mobileApps/$appID"
+
+    try {
+
+        if(!$JSON){
+
+        write-host "No JSON was passed to the function, provide a JSON variable" -f Red
+        break
+
+        }
+
+        if(!$appID){
+
+        write-host "No Application ID was passed to the function, provide the Application ID of the app you want to update" -f Red
+        break
+
+        }
+
+
+        if($IconURL){
+
+        write-verbose "Icon specified: $IconURL"
+
+            if(!(test-path "$IconURL")){
+
+            write-host "Icon Path '$IconURL' doesn't exist..." -ForegroundColor Red
+            Write-Host "Please specify a valid path..." -ForegroundColor Red
+            Write-Host
+            break
+
+            }
+
+        $iconResponse = Invoke-WebRequest "$iconUrl"
+        $base64icon = [System.Convert]::ToBase64String($iconResponse.Content)
+        $iconExt = ([System.IO.Path]::GetExtension("$iconURL")).replace(".","")
+        $iconType = "image/$iconExt"
+
+        Write-Verbose "Updating JSON to add Icon Data"
+
+        $U_JSON = ConvertFrom-Json $JSON
+
+        $U_JSON.largeIcon.type = "$iconType"
+        $U_JSON.largeIcon.value = "$base64icon"
+
+        $JSON = ConvertTo-Json $U_JSON
+
+        Write-Verbose $JSON
+
+        Test-JSON -JSON $JSON
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($App_resource)"
+        Invoke-RestMethod -Uri $uri -Method Patch -ContentType "application/json" -Body $JSON -Headers $authToken
+
+        }
+
+        else {
+
+        Test-JSON -JSON $JSON
+
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($App_resource)"
+        Invoke-RestMethod -Uri $uri -Method Patch -ContentType "application/json" -Body $JSON -Headers $authToken
+
+        }
+
+    }
+
+    catch {
+
+    $ex = $_.Exception
+    $errorResponse = $ex.Response.GetResponseStream()
+    $reader = New-Object System.IO.StreamReader($errorResponse)
+    $reader.BaseStream.Position = 0
+    $reader.DiscardBufferedData()
+    $responseBody = $reader.ReadToEnd();
+    Write-Host "Response content:`n$responseBody" -f Red
+    Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+    write-host
+    break
+
+    }
+
+}
+
+####################################################
+
+#region Authentication
+
+write-host
+
+# Checking if authToken exists before running authentication
+if($global:authToken){
+
+    # Setting DateTime to Universal time to work in all timezones
+    $DateTime = (Get-Date).ToUniversalTime()
+
+    # If the authToken exists checking when it expires
+    $TokenExpires = ($authToken.ExpiresOn.datetime - $DateTime).Minutes
+
+        if($TokenExpires -le 0){
+
+        write-host "Authentication Token expired" $TokenExpires "minutes ago" -ForegroundColor Yellow
+        write-host
+
+            # Defining User Principal Name if not present
+
+            if($User -eq $null -or $User -eq ""){
+
+            $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+            Write-Host
+
+            }
+
+        $global:authToken = Get-AuthToken -User $User
+
+        }
+}
+
+# Authentication doesn't exist, calling Get-AuthToken function
+
+else {
+
+    if($User -eq $null -or $User -eq ""){
+
+    $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+    Write-Host
+
+    }
+
+# Getting the authorization token
+$global:authToken = Get-AuthToken -User $User
+
+}
+
+#endregion
+
+# Update the Folder used to install the application
+$installLocation = "$env:ProgramFiles\CustomFolder"
+
+$name = "My Custom App"
+
+# Get the current application in the 
+$inTuneApp = Get-IntuneApplication -Name $name
+
+# Build the update JSON 
+$appID = $inTuneApp.id
+$odataType = $inTuneApp.'@odata.type'
+$commandLine = "INSTALLLOCATION=\`"$installLocation\`""
+$developer = $inTuneApp.developer
+$displayName = $inTuneApp.displayName
+$informationUrl = $inTuneApp.informationUrl
+$isFeatured = (($inTuneApp.isFeatured).ToString()).toLower()
+$notes = $inTuneApp.notes
+$owner = $inTuneApp.owner
+$privacyInformationUrl = $inTuneApp.privacyInformationUrl
+$publisher = $inTuneApp.publisher
+$UpdateApp = @"
+
+{
+"`@odata.type":"$odataType",
+"commandLine":"$commandLine",
+"description":"$description",
+"developer":"$developer",
+"displayName":"$displayName",
+"informationUrl":$informationUrl,
+"isFeatured":$isFeatured,
+"notes":"$notes",
+"owner":"$owner",
+"privacyInformationUrl":$privacyInformationUrl,
+"publisher":"$publisher"
+}
+
+"@
+
+write-host "Updating" $inTuneApp.displayName -ForegroundColor Yellow
+$Update_IntuneApp = Update-InTuneApplication -JSON $UpdateApp -appID $appID
+
+$inTuneApp = Get-IntuneApplication -Name $name
+
+if ($inTuneApp.commandLine -eq $commandLine) {
+    Write-Host "The Command LIne for $displayName has been successfully updated!" -ForegroundColor Green
+}
+

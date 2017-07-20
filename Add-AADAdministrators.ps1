aacf21792acad54cc2916e4393d27b3d4ab36701
SAMUEL S. GRUMMONS <ssgrummo@us.ibm.com>
2017-07-20 09:59:34 -0700

    Added New Script to Repository
    This script will add a users from a .csv file to an Azure Active
    Directory administrative role.

---
+++ Add-AADAdministrators.ps1
@@ -0,0 +1,26 @@
+﻿<#
+This script adds existing Azure Active Directory Users to an Azure AD Administrative Role from a CSV file.
+All that is required is that the CSV file has a column named "Email" that contains valid Azure Active Directory users.
+The $csvFile and $roleName variables can be edited as desired.
+#>
+
+$csvFile = "C:\Azure\Users.csv"
+$roleName = "User Account Administrator"
+$role = Get-AzureADDirectoryRole | Where {$_.DisplayName -eq $roleName}
+$userRoleMembers =  Get-AzureADDirectoryRoleMember -ObjectId $userRole.ObjectID
+$users = Import-Csv $csvFile
+Foreach ($user in $users) {
+    If ($UserRoleMembers.UserPrincipalName -notcontains $user.Email) {
+        Write-Host $user.Email
+        try {
+            $AADUser = Get-AzureADUser -ObjectId $user.Email
+            } 
+        catch {
+            Write-Host $user.Email " is not found in Azure Active Directory."
+            Write-Host "Skipping this user."
+            continue
+        }
+        Write-Host "Adding " $user.Email " to role " $role.DisplayName
+        Add-AzureADDirectoryRoleMember -ObjectId $role.ObjectId -RefObjectID $AADUser.ObjectId
+    }
+}
